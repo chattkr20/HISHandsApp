a516c5a77d7fc6a61c8202c60c56af74ad9c6f4d
chattkr20 <chattkr20@gmail.com>
2022-10-01 15:13:36 -0500

    initial design for questionnaire, still need to finish backend and final page

---
+++ HISHandsApp/ResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultsViewController.swift
+//  HISHandsApp
+//
+//  Created by Krish Chatterjee on 10/1/22.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HISHandsApp/ViewController.swift
+++ HISHandsApp/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var answer1Label: UITextField!
+    @IBOutlet weak var answer2Label: UITextField!
+    @IBOutlet weak var answer3Label: UITextField!
+    @IBOutlet weak var answer4Label: UITextField!
+    @IBOutlet weak var answer5Slider: UISlider!
+    @IBOutlet weak var answer6Label: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
