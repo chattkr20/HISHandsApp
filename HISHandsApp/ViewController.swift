0eeda697520a6fbccbfba69e8f82346743706328
chattkr20 <chattkr20@gmail.com>
2022-10-01 12:49:32 -0500

    Initial Commit

---
+++ HISHandsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HISHandsApp
+//
+//  Created by Krish Chatterjee on 10/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
